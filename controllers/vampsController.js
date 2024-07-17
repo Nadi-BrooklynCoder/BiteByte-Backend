28c617f70d39c976d0d51650af45a17922e00316
Diamond <dbrown@pursuit.org>
2024-07-17 15:16:07 -0400

    Update page backend completed

--- controllers/vampsController.js
+++ controllers/vampsController.js
@@ -1,25 +1,38 @@
-const express = require('express')
-const vampires = express.Router()
-const  {getAllVampires, getOneVampire}  = require('../queries/vamp')
+const express = require('express');
+const vampires = express.Router();
+const  {getAllVampires, getOneVampire, updateVampireInformation}  = require('../queries/vamp');
 
 
 // http:localhost3333/vampires
 vampires.get("/", async (req, res) => {
-    const allVampires = await getAllVampires()
+    const allVampires = await getAllVampires();
     if (allVampires[0]) {
-        res.status(200).json(allVampires)
+        res.status(200).json(allVampires);
     } else {
-        res.status(500).json({ error: "Internal Server Error" })
+        res.status(500).json({ error: "Internal Server Error" });
     }
 });
 
 vampires.get('/:id', async (req, res)=>{
     const { id } = req.params;
-    const oneVampire = await getOneVampire(id)
+    const oneVampire = await getOneVampire(id);
     if(oneVampire.id){
-        res.status(200).json(oneVampire)
+        res.status(200).json(oneVampire);
     }else{
-        res.status(404).json({error: "Vampire Can Not Be Found"})
+        res.status(404).json({error: "Vampire Can Not Be Found"});
+    }
+})
+
+vampires.put('/:id', async (req,res)=>{
+    const newInfo = req.body;
+    const { id } = req.params;
+    const updateVampireInfo = await updateVampireInformation(id, newInfo);
+    if(updateVampireInfo.id){
+        res.status(200).json(updateVampireInfo);
+        // res.status(200).json({ message: "Vampire information updated successfully" });
+
+    }else{
+        res.status(404).json({ error: "Vampire Can Not Be Found" });
     }
 })
 
--- queries/vamp.js
+++ queries/vamp.js
@@ -13,10 +13,32 @@ const getAllVampires = async () => {
 const getOneVampire = async (id) => {
     try {
         const oneVamp = await db.one("SELECT * FROM vampires WHERE id=$1", id)
-return oneVamp
+        return oneVamp
     } catch (error) {
         return error
     }
 }
 
-module.exports = { getAllVampires, getOneVampire }; 
+const updateVampireInformation = async (id, newInfo) => {
+    try {
+        const updatedInfo = await db.one(
+            "UPDATE vampires SET name=$1, date_turned=$2, location=$3, age=$4, main_diet=$5, power=$6, is_dangerous=$7 WHERE id=$8 RETURNING *",
+            [
+                newInfo.name,
+                newInfo.date_turned,
+                newInfo.location,
+                newInfo.age,
+                newInfo.main_diet,
+                newInfo.power,
+                newInfo.is_dangerous,
+                id
+            ]
+        );
+        return updatedInfo;
+    } catch (error) {
+        return error;
+    }
+};
+
+
+module.exports = { getAllVampires, getOneVampire, updateVampireInformation }; 
